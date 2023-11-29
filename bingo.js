eb417f39f0c948b2f3753a297536242dc328397e
Juan Antonio <xbluuz@gmail.com>
2023-11-29 08:50:22 +0100

    Arreglo de errores

--- bingo.js
+++ bingo.js
@@ -1,24 +1,24 @@
-var repetidas = []
-var bolita = 0
+var repetidas = [];
+var bolita = 0;
 function bola() {
     var bolita = Math.floor((Math.random() * 75) + 1)
 
-
     if (repetidas.indexOf(bolita) == -1) {
-        var numero = document.getElementById(String(bolita))
-        numero.className = "bal selecionado"
-        repetidas.push(bolita)
+        var numero = document.getElementById(String(bolita));
+        numero.className = "bal selecionado";
+        repetidas.push(bolita);
         document.getElementById('numero').textContent = bolita;
+        temp1 = setTimeout("bola()", 500);
     } else {
-        bola()
-        console.log("la bolita ya esta " + bolita)
+        bola();
+        console.log("la bolita ya esta " + bolita);
     }
-    temp1 = setTimeout(("bola()"), 5000)
-    start.style.display = "none"
-    para.style.display = "block"
+    
+    start.style.display = "none";
+    para.style.display = "block";
 }
 function pausa() {
-    clearTimeout(temp1)
-    start.style.display = "block"
-    para.style.display = "none"
+    clearTimeout(temp1);
+    start.style.display = "block";
+    para.style.display = "none";
 }
