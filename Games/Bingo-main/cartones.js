3bdaf26a08ab7d5a009f71a2c896755d4e5b0c52
Juan Antonio <xbluuz@gmail.com>
2023-12-11 09:11:45 +0100

    a

--- Games/Bingo-main/cartones.js
+++ Games/Bingo-main/cartones.js
@@ -1,6 +1,6 @@
 const bingoNumbers = Array.from({ length: 75 }, (_, i) => i + 1);
 let remainingNumbers = [...bingoNumbers];
-
+    
 console.log("bola")
 function getRandomNumber() {
     const randomIndex = Math.floor(Math.random() * remainingNumbers.length);
@@ -10,11 +10,9 @@ function getRandomNumber() {
 function createBingoBoard() {
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
-            let number = getRandomNumber();
             const ball = document.createElement("div");
             ball.className = "ball";
-            ball.id = number;
-            ball.textContent = number;
+            ball.textContent = getRandomNumber();
             document.getElementById("bingo").appendChild(ball);
         }
     }
@@ -29,6 +27,5 @@ document.getElementById("bingo").addEventListener("click", function (event) {
         event.target.classList.toggle("selecionado");
     }
 });
-
 
 document.getElementById("start").addEventListener("click", startGame);
